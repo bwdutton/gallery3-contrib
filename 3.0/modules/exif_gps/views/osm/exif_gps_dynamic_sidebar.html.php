3436b077ef9d24f89f55af31290e7a81058a72ad
chirale <chirale@gmail.com>
2020-12-06 22:54:07 +0100

    Bugfix on photo browse (minimap over img).

--- 3.0/modules/exif_gps/views/osm/exif_gps_dynamic_sidebar.html.php
+++ 3.0/modules/exif_gps/views/osm/exif_gps_dynamic_sidebar.html.php
@@ -5,12 +5,35 @@
 <div id="wrapper">
   <div id="map" style="width: 100%; height: 214px;"></div>
 </div>
+<style>
+.ui-widget-overlay.ui-front:visible {
+  z-index: 998;
+}
+.ui-widget-overlay.ui-front:visible img {
+  z-index: 999;
+}
+</style>
 <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
    integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
    crossorigin=""></script><script>
 
+   var zoomImg = document.getElementsByClassName('g-fullsize-link');
+   zoomImg[0].addEventListener('click', toggleMiniMap, false);
+
 function onEachFeature(feature, layer) {
    // do nothing
+}
+
+function toggleMiniMap() {
+    var miniMap = document.getElementById('map');
+    if (document.getElementById('g-fullsize-overlay')) {
+        miniMap.style.display = '';
+        document.getElementsByTagName('body')[0].removeEventListener('click', toggleMiniMap);
+    }
+    else {
+        miniMap.style.display = 'none';
+        window.exifGpsOsmSidebarMiniMap = document.getElementsByTagName('body')[0].addEventListener('click', toggleMiniMap, false);
+    }
 }
 
 var map = L.map('map').setView([<?=$longitude; ?>, <?=$latitude; ?>], 13);
